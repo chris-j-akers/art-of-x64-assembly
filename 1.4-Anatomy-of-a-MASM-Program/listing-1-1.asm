76ef0127e61e60239d161db8ecfc94cc6b728808
Cakers <chris.j.akers@gmail.com>
2024-01-03 14:50:37 +0000

    first two listings

---
+++ 1.4-Anatomy-of-a-MASM-Program/listing-1-1.asm
@@ -0,0 +1,12 @@
+; An empty program on page 5 :)
+
+        .code
+
+main    PROC
+
+        xor rax, rax
+        ret
+
+main    ENDP
+
+        END
---
+++ 1.6-Running-your-first-masm-cpp-hybrid-program/listing-1-3.asm
@@ -0,0 +1,14 @@
+; Called from CPP listing 1-2.cpp
+
+        .CODE
+        option casemap:none
+
+        public asmFunc
+
+asmFunc         PROC
+
+                xor     rax, rax
+                ret
+
+asmFunc         ENDP
+                END
